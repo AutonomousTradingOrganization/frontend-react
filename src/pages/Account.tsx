532d780aa04fa92b837432d15b596699f9db7270
Boyquotes <webmaster@linux-live-cd.org>
2024-07-17 10:22:06 +0000

    Deploy on github pages

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -60,7 +60,7 @@ export function Account() {
                             onClick={async () => {
                                 if (anchorWallet.publicKey) {
                                     setSendingTransaction(true);
-                                    const initResult = await initializeAccount(anchorWallet, {data}, {age});
+                                    const initResult = await initializeAccount(anchorWallet, data, age);
                                     setTransactionHash(initResult);
                                     setSendingTransaction(false);
                                     console.log("initResult");
