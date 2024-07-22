1b76b4bb85dd839f2ea81367f65531b41ac4ac1c
Boyquotes <webmaster@linux-live-cd.org>
2024-07-22 10:47:44 +0000

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 git add . && git commit -m"$1" && git push
-bun run build && sed -i "s#/static#/frontend-react-build/static#g" build/index.html && rm -rf ../ato-frontend-react-build/* && cp -r build/* ../ato-frontend-react-build/ && cd ../ato-frontend-react-build && date > README.md && git add . && git commit -m"new deploy" && git push && cd -
+bun run build && rm -rf ../ato-frontend-react-build/* && cp -r build/* ../ato-frontend-react-build/ && cd ../ato-frontend-react-build && date > README.md && git add . && git commit -m"new deploy" && git push && cd -
+#&& sed -i "s#/static#/frontend-react-build/static#g" build/index.html 
+#&& rm -rf ../ato-frontend-react-build/* 
+#&& cp -r build/* ../ato-frontend-react-build/ &&
+#cd ../ato-frontend-react-build && date > README.md && git add . && git commit -m"new deploy" && git push && cd -
 
