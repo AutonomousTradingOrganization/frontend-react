4d0f8f80a24efb52121b6e0e9c2a04e0ee2fa381
Boyquotes <webmaster@linux-live-cd.org>
2024-07-22 13:03:33 +0000

    Create proposal form, add state Get last index proposal Fix value of tail index

--- src/helpers/solana.helper.ts
+++ src/helpers/solana.helper.ts
@@ -503,8 +503,9 @@ export const getInitializeAccountTransaction = async (publicKey: PublicKey, data
 export const getCreateProposalAto = async (publicKey: PublicKey, title: String, description: String, mode: BN, threshold: BN, deadline: BN): Promise<Transaction | null> => {
     try {
     const tailIndex = await programAto2.account.atoData.fetch(atoUser.publicKey);
+    const tailIndexProposal = tailIndex.proposalIndexTail;
     // const tailIndexProposal = tailIndex.proposalIndexTail.valueOf();
-    const tailIndexProposal = 2;
+    // const tailIndexProposal = 2;
     console.log(tailIndex);
     console.log(tailIndexProposal);
     const propsIndexBuffer = Buffer.allocUnsafe(2);
